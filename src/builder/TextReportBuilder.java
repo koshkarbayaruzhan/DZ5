48a68c3621ebbe24c0192defd618459162571055
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 09:35:46 +0500

    Update TextReportBuilder.java

--- src/builder/TextReportBuilder.java
+++ src/builder/TextReportBuilder.java
@@ -1,4 +1,22 @@
 package builder;
 
-public class TextReportBuilder {
+public class TextReportBuilder implements IReportBuilder {
+
+    private Report report = new Report();
+
+    public void setHeader(String header) {
+        report.setHeader("TEXT HEADER: " + header);
+    }
+
+    public void setContent(String content) {
+        report.setContent("TEXT CONTENT: " + content);
+    }
+
+    public void setFooter(String footer) {
+        report.setFooter("TEXT FOOTER: " + footer);
+    }
+
+    public Report getReport() {
+        return report;
+    }
 }
