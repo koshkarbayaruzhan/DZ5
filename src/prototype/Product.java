414dc928df73490848ca3863628053edabd63994
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 09:37:03 +0500

    Update Product.java

--- src/prototype/Product.java
+++ src/prototype/Product.java
@@ -1,4 +1,28 @@
 package prototype;
 
-public class Product {
+public class Product implements Cloneable {
+
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public Product clone() {
+        try {
+            return (Product) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " x" + quantity + " = " + price;
+    }
 }
