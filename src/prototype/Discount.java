c655409fb01c84b505280a6505f4bde51d5f46ad
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 09:37:22 +0500

    Update Discount.java

--- src/prototype/Discount.java
+++ src/prototype/Discount.java
@@ -1,4 +1,26 @@
 package prototype;
 
-public class Discount {
+public class Discount implements Cloneable {
+
+    private String name;
+    private double value;
+
+    public Discount(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public Discount clone() {
+        try {
+            return (Discount) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + value + "%";
+    }
 }
