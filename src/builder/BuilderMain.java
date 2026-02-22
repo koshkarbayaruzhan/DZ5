33226410bf193bd84c9701b995b0170312d01ea6
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 09:36:42 +0500

    Update BuilderMain.java

--- src/builder/BuilderMain.java
+++ src/builder/BuilderMain.java
@@ -1,4 +1,21 @@
-package builder;
+package main;
+
+import builder.*;
 
 public class BuilderMain {
+
+    public static void main(String[] args) {
+
+        ReportDirector director = new ReportDirector();
+
+        IReportBuilder textBuilder = new TextReportBuilder();
+        director.constructReport(textBuilder);
+        System.out.println(textBuilder.getReport());
+
+        System.out.println("----------------");
+
+        IReportBuilder htmlBuilder = new HtmlReportBuilder();
+        director.constructReport(htmlBuilder);
+        System.out.println(htmlBuilder.getReport());
+    }
 }
