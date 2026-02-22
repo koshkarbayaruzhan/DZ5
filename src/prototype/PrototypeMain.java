71643f66deba3c36d1ead443d32beb6c7507170f
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 09:38:15 +0500

    Update PrototypeMain.java

--- src/prototype/PrototypeMain.java
+++ src/prototype/PrototypeMain.java
@@ -1,4 +1,22 @@
-package prototype;
+package main;
+
+import prototype.*;
 
 public class PrototypeMain {
+
+    public static void main(String[] args) {
+
+        Order base = new Order();
+        base.addProduct(new Product("Laptop", 350000, 1));
+        base.addDiscount(new Discount("New Year", 10));
+        base.setDeliveryCost(2000);
+        base.setPaymentMethod("Card");
+
+        Order clone = base.clone();
+        clone.setPaymentMethod("Cash");
+
+        System.out.println("ORIGINAL:\n" + base);
+        System.out.println("------------");
+        System.out.println("CLONE:\n" + clone);
+    }
 }
