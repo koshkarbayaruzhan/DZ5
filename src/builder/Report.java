cc22822285378ee16e8cd27dba5c77273022ae1d
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 09:35:26 +0500

    Update Report.java

--- src/builder/Report.java
+++ src/builder/Report.java
@@ -1,4 +1,25 @@
 package builder;
 
 public class Report {
+
+    private String header;
+    private String content;
+    private String footer;
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
+
+    @Override
+    public String toString() {
+        return header + "\n" + content + "\n" + footer;
+    }
 }
